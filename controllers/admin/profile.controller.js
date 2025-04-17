78c615c8fdb16064d115347de983e8b234e0e7ec
Danh13320031 <danh13320031@gmail.com>
2025-04-17 21:35:41 +0700

    add update profile feature

--- controllers/admin/profile.controller.js
+++ controllers/admin/profile.controller.js
@@ -1,3 +1,9 @@
+import bcrypt from 'bcrypt';
+import alertMessageHelper from '../../helpers/alertMessagge.helper.js';
+import accountModel from '../../models/account.model.js';
+
+const saltRounds = 10;
+
 // GET: /admin/profile     --Tới trang thông tin tài khoản
 const profile = async (req, res) => {
   res.render('./admin/pages/profile/profile.view.ejs', {
@@ -5,8 +11,35 @@ const profile = async (req, res) => {
   });
 };
 
+// GET: /admin/profile/update     --Tới trang chỉnh sửa thông tin tài khoản
+const updateProfileGet = async (req, res) => {
+  res.render('./admin/pages/profile/update.view.ejs', {
+    pageTitle: 'Chỉnh sửa thông tin tài khoản',
+  });
+};
+
+// PATCH: /admin/profile/update?_method=PATCH     --Cập nhật thông tin tài khoản
+const updateProfilePatch = async (req, res) => {
+  try {
+    const id = res.locals.account._id;
+    const hassPassword = await bcrypt.hash(req.body.password, saltRounds);
+
+    if (req.body.password) req.body.password = hassPassword;
+
+    await accountModel.findByIdAndUpdate(id, req.body);
+    alertMessageHelper(req, 'alertSuccess', 'Cập nhật thành công');
+  } catch (err) {
+    console.log('Create product fail: ', err);
+    alertMessageHelper(req, 'alertFailure', 'Cập nhật thất bại');
+  } finally {
+    res.redirect('back');
+  }
+};
+
 const profileController = {
   profile,
+  updateProfileGet,
+  updateProfilePatch,
 };
 
 export default profileController;
