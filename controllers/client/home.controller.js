aa988a7ef7779d36f731486ad4639f8ee714b384
Danh13320031 <danh13320031@gmail.com>
2025-04-20 11:09:12 +0700

    show product category ui client (unfinished)

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,7 +1,15 @@
+import categoryTreeHelper from '../../helpers/categoryTree.helper.js';
+import categoryModel from '../../models/category.model.js';
+
 // GET: /
-const home = (req, res) => {
+const home = async (req, res) => {
+  const find = { status: 'active', deleted: false };
+  const categoryList = await categoryModel.find(find);
+  const categoryTree = categoryTreeHelper(categoryList);
+
   res.render('./client/pages/home/index.view.ejs', {
     pageTitle: 'Trang chủ',
+    categoryTree: categoryTree,
   });
 };
 
