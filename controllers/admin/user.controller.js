34d4ef6bc8c93026fe4fb00b18db7d3cdfc0cda5
Danh13320031 <danh13320031@gmail.com>
2025-10-10 23:41:12 +0700

    complete change status filter feature for user list ui admin

--- controllers/admin/user.controller.js
+++ controllers/admin/user.controller.js
@@ -3,6 +3,7 @@ import searchHelper from '../../helpers/search.helper.js';
 import statusFilterHelper from '../../helpers/statusFilter.helper.js';
 import userModel from '../../models/user.model.js';
 
+// GET: /admin/users
 const user = async (req, res) => {
   const find = { deleted: false };
 
@@ -47,8 +48,33 @@ const user = async (req, res) => {
   }
 };
 
+// PATCH: /admin/users/change-status/:status/:id?_method=PATCH     --Đổi trạng thái người dùng
+const changeStatusUser = async (req, res) => {
+  try {
+    const { id, status } = req.params;
+
+    if (!id || !status) {
+      alertMessageHelper(req, 'alertFailure', 'Cập nhật trạng thái thất bại');
+      res.redirect('back');
+      return;
+    }
+
+    await userModel.findByIdAndUpdate(id, {
+      status: status,
+    });
+    alertMessageHelper(req, 'alertSuccess', 'Cập nhật trạng thái thành công');
+  } catch (err) {
+    console.log('Update product fail: ', err);
+    alertMessageHelper(req, 'alertFailure', 'Cập nhật trạng thái thất bại');
+  } finally {
+    res.redirect('back');
+    return;
+  }
+};
+
 const userController = {
   user,
+  changeStatusUser,
 };
 
 export default userController;
