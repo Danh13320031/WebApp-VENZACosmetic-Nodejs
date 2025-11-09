0ed7823cccbb94e1fd0cff2b42a7dfb8764ec271
Danh13320031 <danh13320031@gmail.com>
2025-11-09 11:36:39 +0700

    complete view flash sale product feature client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,8 +1,9 @@
-import { notFoundPage } from '../../constants/constant.js';
+import { timezone, productFlashSaleDurationMsConst } from '../../constants/constant.js';
 import categoryTreeHelper from '../../helpers/categoryTree.helper.js';
 import handleErrorHelper from '../../helpers/handleError.helper.js';
 import productModel from '../../models/product.model.js';
 import productCategoryModel from '../../models/productCategory.model.js';
+import moment from '../../node_modules/moment/moment.js';
 
 // GET: /
 const home = async (req, res) => {
@@ -26,12 +27,50 @@ const home = async (req, res) => {
       });
     }
 
+    const productFeatureList = productList.filter((product) => {
+      if (product.featured === '1') return product;
+    });
+
+    const productSaleList = productList.filter((product) => {
+      if (product.discount !== 0) return product;
+    });
+
+    const productFlashSaleList = productList.filter((product) => {
+      if (
+        product.discount !== 0 &&
+        product.discountExpiredAt !== null &&
+        product.discountExpiredAt - moment(Date.now()).tz(timezone).toDate() <
+          productFlashSaleDurationMsConst
+      )
+        return product;
+    });
+
+    const productDiscountExpiredAtMax = await productModel
+      .find(find)
+      .sort({ discountExpiredAt: 'desc' })
+      .limit(1);
+
+    const duration = moment.duration(
+      productDiscountExpiredAtMax[0].discountExpiredAt - moment(Date.now()).tz(timezone).toDate()
+    );
+
+    const productFlashSaleDuration = {
+      dayNumber: duration.days(),
+      hourNumber: duration.hours(),
+      minuteNumber: duration.minutes(),
+      secondNumber: duration.seconds(),
+    };
+
     res.render(
       './client/pages/home/index.view.ejs',
       {
         pageTitle: 'Trang chủ',
         categoryTree: categoryTree,
+        productFeatureList,
+        productSaleList,
+        productFlashSaleList,
         productList: productList,
+        productFlashSaleDuration,
       },
       (err, html) => {
         if (err) handleErrorHelper(req, res, err);
